822851c4ccb2d2f2cebbd0ff6550c1845903e1b9
HotDamnCoder <marcusb2kl@gmail.com>
2020-10-03 23:14:24 +0300

    Tried to do complete it

--- fitness.py
+++ fitness.py
@@ -1,18 +1,57 @@
-import myfitnesspal
 import PyPDF2
-from urllib.request import urlretrieve
-import datetime
+import requests
 from bs4 import BeautifulSoup
-import bs4
-import json
-import re
 
-now = datetime.datetime.now(datetime.timezone.utc)
-now = str(now).split('.')[0] + 'Z'
-now = now.replace(' ', 'T')
-client = myfitnesspal.Client('***REMOVED***')
-session = client.session
-url_1 = 'https://nrg.edu.ee/et/toitlustamine'
+MFP_LOGIN_URL = 'https://www.myfitnesspal.com/account/login'
+MFP_LOGIN_PAYLOAD = {'utf8': '✓',
+                     'authenticity_token': '',
+                     'username': '***REMOVED***',
+                     'password': '***REMOVED***',
+                     'remember_me': '1'}
+MFP_NEW_FOOD_URL = 'https://www.myfitnesspal.com/food/new'
+MFP_NEW_FOOD_PAYLOAD = {'utf8': '✓',
+                        'authenticity_token': '',
+                        'date': '2020-10-03',
+                        'food[brand]': 'Generic',
+                        'food[description]': 'sssasa',
+                        'weight[serving_size]': '100g',
+                        'servingspercontainer': '1',
+                        'nutritional_content[calories]': '750',
+                        'nutritional_content[sodium]': '',
+                        'nutritional_content[fat]'':'','
+                        'nutritional_content[potassium]': '',
+                        'nutritional_content[saturated_fat]': '',
+                        'nutritional_content[carbs]': '',
+                        'nutritional_content[polyunsaturated_fat]': '',
+                        'nutritional_content[fiber]': '',
+                        'nutritional_content[monounsaturated_fat]': '',
+                        'nutritional_content[sugar]': '',
+                        'nutritional_content[trans_fat]': '',
+                        'nutritional_content[protein]': '',
+                        'nutritional_content[cholesterol]': '',
+                        'nutritional_content[vitamin_a]': '',
+                        'nutritional_content[calcium]': '',
+                        'nutritional_content[vitamin_c]': '',
+                        'nutritional_content[iron]': '',
+                        'addtodiary': 'yes',
+                        'food_entry[quantity]': '1.0',
+                        'food_entry[meal_id]': '1',
+                        'preserve_exact_description_and_brand': 'true',
+                        'continue': 'Save'}
+MENU_URL = 'https://nrg.edu.ee/et/toitlustamine'
+
+session = requests.session()
+
+login_page = session.get(MFP_LOGIN_URL)
+auth_token = BeautifulSoup(login_page.text, features="html.parser").find('input', {'name': 'authenticity_token'}).attrs[
+    'value']
+MFP_LOGIN_PAYLOAD['authenticity_token'] = auth_token
+login_response = session.post(MFP_LOGIN_URL, MFP_LOGIN_PAYLOAD)
+new_food_page = session.get(MFP_NEW_FOOD_URL)
+auth_token = BeautifulSoup(new_food_page.text, features="html.parser").find('input', {'name': 'authenticity_token'}).attrs['value']
+MFP_NEW_FOOD_PAYLOAD['authenticity_token'] = auth_token
+new_food_response = session.post(MFP_NEW_FOOD_URL, MFP_NEW_FOOD_PAYLOAD)
+"""
 url_2 = 'https://api.myfitnesspal.com/v2/diary'
 header = {
     'authorization': 'Bearer ' + client._auth_data['access_token'],
@@ -20,56 +59,4 @@ header = {
     'mfp-user-id': client.user_id,
     'content-type': 'application/json; charset=UTF-8'
     }
-calories = 800
-fat = 32
-carbs = 14
-sugar = 14
-protein = 17
-data_2 = {
-    "items": [
-        {
-            "meal_name": "Lunch",
-            "date": now.split('T')[0],
-            "nutritional_contents":{
-                "fat": fat,
-                "carbohydrates": carbs,
-                "sugar": sugar,
-                "protein": protein,
-                "energy": {
-                    "value": calories, "unit": "calories"
-                    }
-            },
-            "type": "quick_add"
-        }
-
-    ]
-}
-data_2 = json.dumps(data_2)
-response = session.get(url_1)
-html = BeautifulSoup(response.text, features="lxml")
-a = html.find('strong')
-links = [link for link in a.contents if type(link) == bs4.Tag]
-links = [link for link in links if link.text != " " or link.text == ""]
-links = [link.attrs['href'] for link in links]
-names = [link.split('/')[-1] for link in links]
-texts = []
-for link, name in zip(links, names):
-    urlretrieve(link, name)
-for name in names:
-    pdfFileObj = open(name, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    text = [pdfReader.getPage(i).extractText().replace('\n', '') for i in range(pdfReader.numPages-2)]
-    pdfFileObj.close()
-    texts.append(text)
-
-pattern = r'Kokku.+?(?=\s{8})'
-a = [[re.findall(pattern, a) for a in text] for text in texts]
-calories = []
-for x in a:
-    cals = []
-    for b in x:
-        for y in b:
-            y = y.split(',')[0]
-            cals.append(re.split('\\s+?(?=\\d)', y)[1])
-    calories.append(cals)
-print(a)
+"""
