8206f1ed9ff551a1aca0cf70be6723442dbfa41c
HotDamnCoder <marcusb2kl@gmail.com>
2020-10-04 13:36:47 +0300

    Implemented menu scraping

--- fitness.py
+++ fitness.py
@@ -1,6 +1,32 @@
+import datetime
+import io
+import re
+
 import PyPDF2
 import requests
 from bs4 import BeautifulSoup
+
+
+def order_date(date):
+    dates = date.split('-')
+    return '.'.join([dates[-1], dates[1], dates[0]])
+
+
+DAYS = {0: 'E',
+        1: 'T',
+        2: 'K',
+        3: 'N',
+        4: 'R'}
+NUTRITION = {0: 'kcal',
+             1: 'valgud',
+             2: 'rasvad',
+             3: 'süsivesikud'}
+
+DATEFROM = (datetime.date.today() - datetime.timedelta(days=datetime.date.today().weekday()))
+NEXTWEEKDATEFROM = DATEFROM + datetime.timedelta(days=7)
+
+DATEFROM = order_date(str(DATEFROM))
+NEXTWEEKDATEFROM = order_date(str(NEXTWEEKDATEFROM))
 
 USERNAME = '***REMOVED***'
 PASSWORD = '***REMOVED***'
@@ -51,6 +77,30 @@ MFP_NEW_FOOD_SUBMIT_PAYLOAD = {'utf8': '✓',
 MENU_URL = 'https://nrg.edu.ee/et/toitlustamine'
 
 session = requests.session()
+
+menu_page_response = session.get(MENU_URL)
+links = BeautifulSoup(menu_page_response.text, features="html.parser").find_all('a')
+
+menu_links = []
+menu_summaries = []
+for link in links:
+    if DATEFROM in link.text or NEXTWEEKDATEFROM in link.text:
+        if link.attrs['href'] not in menu_links:
+            pdfReader = PyPDF2.PdfFileReader(io.BytesIO(session.get(link.attrs['href']).content))
+            text = ''.join([pdfReader.getPage(i).extractText().replace('\n', '') for i in range(2)])
+            menu_summary = {}
+            i = 0
+            for summary in re.findall('Kokku\\s+.*?(?=\\s{2,})', text):
+                values = [float(nr.replace(',', '.')) for nr in re.search('\\s+.+', summary).group(0).strip().split()]
+                nutritional_values = {}
+                j = 0
+                for value in values:
+                    nutritional_values[NUTRITION[j]] = value
+                    j += 1
+                menu_summary[DAYS[i]] = nutritional_values
+                i += 1
+            menu_summaries.append(menu_summary)
+            menu_links.append(link.attrs['href'])
 
 login_response = session.post(MFP_LOGIN_URL, MFP_LOGIN_PAYLOAD)
 
